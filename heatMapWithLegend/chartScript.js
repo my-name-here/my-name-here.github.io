71c872492c402cc5cb24394f7609d1c6d8c18cf0
CB4747 <123422381+CB4747@users.noreply.github.com>
2025-03-11 17:22:29 -0700

    more adjustments to size and postion

--- heatMapWithLegend/chartScript.js
+++ heatMapWithLegend/chartScript.js
@@ -1,9 +1,9 @@
 // basic framework from class example, edited to work for my needs
 // started with a copy of my bar chart, and edited
 // Set up the SVG container
-const svgWidth = 1550;
-const svgHeight = 600;
-const margin = { top: 50, right: 200, bottom: 100, left: 200 };
+const svgWidth = 1800;
+const svgHeight = 625;
+const margin = { top: 50, right: 220, bottom: 100, left: 150 };
 const width = svgWidth - margin.left - margin.right;
 const height = svgHeight - margin.top - margin.bottom;
 
@@ -151,6 +151,6 @@ d3.csv("https://raw.githubusercontent.com/my-name-here/my-name-here.github.io/re
 		
 
     svg.append("g")
-        .attr("transform", `translate(${width+margin.right/2},0)`)
+        .attr("transform", `translate(${width+margin.right/2+20},0)`)
         .call(legend);
 });
